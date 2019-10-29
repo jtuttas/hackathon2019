b1110bf910c1560326b087d49b6873180ff1a909
Florian <f.wrage@googlemail.com>
2019-10-29 09:55:28 +0100

    to upload student json to firestore

---
+++ firestore/pushDocument.py
@@ -0,0 +1,20 @@
+﻿import firebase_admin
+import google.cloud
+from firebase_admin import credentials, firestore
+import json
+
+# Credentials
+cred = credentials.Certificate("./ServiceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+
+store = firestore.client()
+
+jsonPath = "./students.json"
+collection = "students"
+
+data = json.load(open(jsonPath, "r").read())
+
+for student in data:
+    documentName = student["id"]
+
+store.collection(collection).document(documentName).set(data)
